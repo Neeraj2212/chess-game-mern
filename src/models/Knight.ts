663e253434d265e9092811eab5dffe4ca693f100
Neeraj Patil <patilneeraj2212@gmail.com>
2023-10-08 12:42:49 +0530

    Knights movement and attack

--- src/models/Knight.ts
+++ src/models/Knight.ts
@@ -1,5 +1,6 @@
 import { PieceType, Position, Color } from "@helpers/Constants";
 import { Piece } from "./Piece";
+import { Board } from "./Board";
 
 export class Knight extends Piece {
   image: string;
@@ -11,12 +12,43 @@ export class Knight extends Piece {
     this.image = `assets/images/knight_${color}.png`;
   }
 
-  isValidMove(destination: Position): boolean {
-    return true;
+  isValidMove(destination: Position, board: Board): boolean {
+    this.updatePossibleMoves(board);
+    for (const move of this.possibleMoves) {
+      if (move.x === destination.x && move.y === destination.y) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  updatePossibleMoves(): void {
-    return;
+  updatePossibleMoves(board: Board): void {
+    this.possibleMoves = [];
+    const directions = [
+      { x: 1, y: 2 },
+      { x: 1, y: -2 },
+      { x: -1, y: 2 },
+      { x: -1, y: -2 },
+      { x: 2, y: 1 },
+      { x: 2, y: -1 },
+      { x: -2, y: 1 },
+      { x: -2, y: -1 },
+    ];
+    for (const direction of directions) {
+      const nextPosition: Position = {
+        x: this.position.x + direction.x,
+        y: this.position.y + direction.y,
+      };
+      if (board.isTileValid(nextPosition)) {
+        if (board.isTileOccupied(nextPosition)) {
+          if (board.getPieceAt(nextPosition)?.color !== this.color) {
+            this.possibleMoves.push(nextPosition);
+          }
+        } else {
+          this.possibleMoves.push(nextPosition);
+        }
+      }
+    }
   }
 
   clone(): Piece {
