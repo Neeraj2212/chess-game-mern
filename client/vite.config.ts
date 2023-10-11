878ca58cf94645cc858734fb0d3717ea1364d50e
Neeraj Patil <patilneeraj2212@gmail.com>
2023-10-11 12:48:35 +0530

    Updated client proxy target

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://chess-game-backend-yy4g.onrender.com",
         changeOrigin: true,
       },
     },
